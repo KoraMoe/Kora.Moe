a3fbb999fa57722513cb577056894e6448443761
BackRunner <dev@backrunner.top>
2025-09-01 02:27:13 +0800

    fix: admin-user

--- packages/frontend/src/pages/admin-user.vue
+++ packages/frontend/src/pages/admin-user.vue
@@ -258,6 +258,7 @@ const ips = ref(result.ips);
 const ap = ref<any>(null);
 const approved = ref(info.value.approved);
 const moderator = ref(info.value.isModerator);
+const silenced = ref(info.value.isSilenced);
 const suspended = ref(info.value.isSuspended);
 const isSystem = ref(user.value.host == null && user.value.username.includes('.'));
 const moderationNote = ref(info.value.moderationNote);
